70bb04a4716cd4e91c0e68753f96d94edf6294b4
anna-kushnir <anna.kushnir.1008@gmail.com>
2021-09-23 10:06:37 +0300

    output fields have been changed

--- OP_Lab1_Cpp/OP_Lab1_Cpp.cpp
+++ OP_Lab1_Cpp/OP_Lab1_Cpp.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 int main() {
+	setlocale(LC_ALL, "");
 	double a, b;    //катети
 	double S;       //площа
-	cout << "a = "; cin >> a;
-	cout << "b = "; cin >> b;
-	S = (a * b) / 2;
-	cout << "S = ";
+	cout << "Введiть довжину першого катета: "; cin >> a;
+	cout << "Введiть довжину другого катета: "; cin >> b;
+	S = a * b / 2;
+	cout << "\n";
+	cout << "Площа прямокутного трикутника: ";
 	cout << S;
 	cout << "\n";
 	system("pause");
