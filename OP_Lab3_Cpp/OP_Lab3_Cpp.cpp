73b942507bb5fc5fe916d9fae28fa44f2c4f69e0
anna-kushnir <anna.kushnir.1008@gmail.com>
2021-10-10 11:42:22 +0300

    upload 3rd lab

---
+++ OP_Lab3_Cpp/OP_Lab3_Cpp.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	float x;       // початкове дане
+	long n;        // лічильник
+	float a;       // n-ий член послідовності
+	float Sum;     // результат
+	cout << "Введiть значення x: "; cin >> x;
+	n = 1;
+	a = x / 2;
+	Sum = a;
+	while (n < 11)
+	{
+		n = n + 1;
+		a = (a * x) / (2 * n * (2 * n - 1));
+		Sum = Sum + a;
+	}
+	while (abs(a) >= 0.00001)
+	{
+		n = n + 1;
+		a = (a * x) / (2 * n * (2 * n - 1));
+		Sum = Sum + a;
+	}
+	cout << "Сума k членiв послiдовностi, якi задовольняють умову: " << endl;
+	printf("Sum = %.5f\n", Sum);
+	system("pause");
+}
