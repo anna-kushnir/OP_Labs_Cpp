b2d7755ae6e1e21850ddcc84589db15314dfeaf8
anna-kushnir <anna.kushnir.1008@gmail.com>
2021-10-13 13:43:00 +0300

    1

--- OP_Lab4_Cpp/OP_Lab4_Cpp.cpp
+++ OP_Lab4_Cpp/OP_Lab4_Cpp.cpp
@@ -7,14 +7,14 @@ int main()
 	long n;        // к≥льк≥сть член≥в посл≥довност≥
 	float a;       // n-ий член посл≥довност≥
 	float Sum;     // результат
-	cout << "¬ведiть значенн€ n: "; cin >> n;
+	cout << "¬вед≥ть значенн€ n: "; cin >> n;
 	a = 2;
 	Sum = a;
 	for (int i = 2; i <= n; i=i+1) {
 		a = (a * a) / (a + 3);
 		Sum = Sum + a;
 	}
-	cout << "—ума n членiв послiдовностi: ";
+	cout << "—ума n член≥в посл≥довност≥: ";
 	cout << Sum << endl;
 	system("pause");
 }
