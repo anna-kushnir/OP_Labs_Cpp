b43acd11c1849be70c8a9f076067d60382437550
anna-kushnir <anna.kushnir.1008@gmail.com>
2021-10-13 13:39:31 +0300

    the 4th lab is done

--- OP_Lab4_Cpp/OP_Lab4_Cpp.cpp
+++ OP_Lab4_Cpp/OP_Lab4_Cpp.cpp
@@ -4,5 +4,17 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "");
-
+	long n;        // к≥льк≥сть член≥в посл≥довност≥
+	float a;       // n-ий член посл≥довност≥
+	float Sum;     // результат
+	cout << "¬ведiть значенн€ n: "; cin >> n;
+	a = 2;
+	Sum = a;
+	for (int i = 2; i <= n; i=i+1) {
+		a = (a * a) / (a + 3);
+		Sum = Sum + a;
+	}
+	cout << "—ума n членiв послiдовностi: ";
+	cout << Sum << endl;
+	system("pause");
 }
