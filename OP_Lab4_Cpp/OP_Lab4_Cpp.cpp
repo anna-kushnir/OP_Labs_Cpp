edc83f2ffbb0eeb7bc28eb845b74c89c3aa8bd47
anna-kushnir <anna.kushnir.1008@gmail.com>
2021-10-20 12:22:45 +0300

    re-created lab4

--- OP_Lab4_Cpp/OP_Lab4_Cpp.cpp
+++ OP_Lab4_Cpp/OP_Lab4_Cpp.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "");
-	unsigned long n;        // к≥льк≥сть член≥в посл≥довност≥
-	float a;       // n-ий член посл≥довност≥
-	float Sum;     // результат
+	unsigned long n;        // кiлькiсть членiв послiдовностi
+	float a;                // n-ий член послiдовностi
+	float Sum;              // результат
 	cout << "¬ведiть значенн€ n: "; cin >> n;
 	a = 2;
 	Sum = a;
-	for (int i = 2; i <= n; i=i+1) {
+	for (int i = 2; i <= n; i = i + 1) {
 		a = (a * a) / (a + 3);
 		Sum = Sum + a;
 	}
