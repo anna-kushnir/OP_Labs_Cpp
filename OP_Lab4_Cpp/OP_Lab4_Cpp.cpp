97870aedd97b7d44a0c90e714adfa6f2dc95432a
anna-kushnir <anna.kushnir.1008@gmail.com>
2021-10-20 12:53:14 +0300

    comment language changed

--- OP_Lab4_Cpp/OP_Lab4_Cpp.cpp
+++ OP_Lab4_Cpp/OP_Lab4_Cpp.cpp
@@ -4,17 +4,17 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "");
-	unsigned long n;        // кiлькiсть членiв послiдовностi
-	float a;                // n-ий член послiдовностi
-	float Sum;              // результат
-	cout << "¬ведiть значенн€ n: "; cin >> n;
+	unsigned long n;        // the number of members of the sequence
+	float a;                // nth member of the sequence
+	float Sum;              // result
+	cout << "Enter the value of n: "; cin >> n;
 	a = 2;
 	Sum = a;
 	for (int i = 2; i <= n; i = i + 1) {
 		a = (a * a) / (a + 3);
 		Sum = Sum + a;
 	}
-	cout << "—ума n членiв послiдовностi: ";
+	cout << "The sum of n members of the sequence: ";
 	cout << Sum << endl;
 	system("pause");
 }
