4ea4853416065c8abc119877a0d073fa4a4db863
anna-kushnir <anna.kushnir.1008@gmail.com>
2021-10-23 23:15:27 +0300

    the lab5 was created

---
+++ OP_Lab5_Cpp/OP_Lab5_Cpp.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	unsigned long m, n;        //natural numbers m and n
+	unsigned long k;           //natural number k (the result)
+	unsigned long i;           //the counter
+	unsigned long Sum_k;       //the sum of digits of the number k
+	cout << "Enter natural number m: "; cin >> m;
+	cout << "Enter natural number n: "; cin >> n;
+	for (k = 1; k < m; k = k + 1)
+	{
+		Sum_k = 0;
+		for (i = k; i > 0; i = i / 10)
+		{
+			Sum_k = Sum_k + i % 10;
+		}
+		if (Sum_k == n)
+			cout << k << endl;
+	}
+	system("pause");
+}
