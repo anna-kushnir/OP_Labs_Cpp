1f2969f4d559dd2734e643f1ea81e53acb5c4ecc
anna-kushnir <anna.kushnir.1008@gmail.com>
2021-12-10 21:34:04 +0200

    global variables have been moved to the main function and now are passed by parameters to the function. and some structure changes

--- OP_Lab7_Cpp/OP_Lab7_Cpp.cpp
+++ OP_Lab7_Cpp/OP_Lab7_Cpp.cpp
@@ -7,54 +7,36 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctime>
 using namespace std;
 
-const int k = 10;
-int A[k];
-void input_a(int[]);
-void output_a(int[]);
-bool check(int[]);
-void change(int);
-void find_q(int[]);
-void replace(int[]);
-
-int main()
-{
-	input_a(A);
-	cout << "Array: ";
-	output_a(A);
-	int R = check(A);
-	change(R);
-	system("pause");
-}
-
-void input_a(int arr[])
+void input_a(float *p, const int m)
 {
 	srand(time(NULL));
-	for (int i = 0; i < k; i++)
+	for (int i = 0; i < m; i++)
 	{
-		arr[i] = rand() % 50 - 9;
+		*p = rand() % 50 - 9;
+		p++;
 	}
 }
 
-void output_a(int arr[])
+void output_a(float *p, const int m)
 {
-	for (int i = 0; i < k; i++)
-		printf("%3d", arr[i]);
+	for (int i = 0; i < m; i++)
+	{
+		cout << float(*p) << "  ";
+		p++;
+	}
 	printf("\n");
 }
 
-bool check(int arr[])
+bool check(float arr[], const int m)
 {
 	bool R = true;
-	for (int i = 1; i < k; i++)
+	for (int i = 1; i < m - 1; i++)
 	{
-		if (arr[i] == 0 || arr[i-1]==0 || arr[i+1]==0)
-		{
-			R = false;
-			break;
-		}
-		if (arr[i] * arr[i] != arr[i - 1] * arr[i + 1])
+		if (arr[i] == 0 || arr[i - 1] == 0 || arr[i + 1] == 0 ||
+			arr[i] * arr[i] != arr[i - 1] * arr[i + 1])
 		{
 			R = false;
 			break;
@@ -63,35 +45,46 @@ bool check(int arr[])
 	return R;
 }
 
-void change(int R)
+float find_q(float arr[])
 {
+	float q = arr[1] / arr[0];
+	return q;
+}
+
+void replace(float arr[], const int n)
+{
+	float am = (arr[0] + arr[n - 1]) / 2;
+	cout << "Arithmetic mean of the first and last: " << am << endl;
+	for (int i = 0; i < n; i++)
+	{
+		if (int(arr[i]) % 2 == 1 || int(arr[i]) % 2 == -1)
+		{
+			arr[i] = am;
+		}
+	}
+}
+
+int main()
+{
+	const int k = 10;
+	float A[k];
+	input_a(A, k);
+	cout << "Array: " << endl;
+	output_a(A, k);
+	bool R = check(A, k);
 	if (R)
 	{
 		cout << endl << "This is a geometric progression" << endl;
-		find_q(A);
+		float q = find_q(A);
+		cout << "Denominator of geometric progression q: " << q << endl;
 	}
 	else
 	{
 		cout << endl << "This is not a geometric progression" << endl;
-		replace(A);
+		replace(A, k);
+		cout << "New Array: " << endl;
+		output_a(A, k);
 	}
-}
-
-void find_q(int arr[])
-{
-	float q = float(arr[1] / arr[0]);
-	cout << "Denominator of geometric progression q: " << q << endl;
-}
-
-void replace(int arr[])
-{
-	float am = float((arr[0] + arr[k - 1]))/2;
-	cout << "Arithmetic mean of the first and last: " << am << endl;
-	for (int i = 0; i < k; i++)
-	{
-		if (arr[i] % 2 == 1 || arr[i] % 2 == -1)
-			arr[i] = float(am);
-	}
-	cout << "New Array: " << endl;
-	output_a(arr);
+	cout << endl;
+	system("pause");
 }
