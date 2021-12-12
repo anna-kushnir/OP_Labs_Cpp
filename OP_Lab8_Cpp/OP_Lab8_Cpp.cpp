c16d2424050481e7d4aa8d20b921c34be44b1b9d
anna-kushnir <anna.kushnir.1008@gmail.com>
2021-12-12 23:11:19 +0200

    the lab8 has been fixed

--- OP_Lab8_Cpp/OP_Lab8_Cpp.cpp
+++ OP_Lab8_Cpp/OP_Lab8_Cpp.cpp
@@ -3,74 +3,94 @@
 #include <iomanip>
 using namespace std;
 
-typedef float Matrix[10][10];
-Matrix A, B;
-int n;
-void input(Matrix);
-void output(Matrix);
-void creation(Matrix);
+float** input(int, int);
+void output(float**, int);
+float** creation(float**, int);
+void deleting(float**, int);
 
 int main()
 {
+	int n;
 	cout << "Enter the dimension (size) of the matrix: ";
 	cin >> n;
-	input(A);
+	float** A, ** B;
+	int maxA;
+	cout << "Enter the maximum possible value of an element from matrix A: ";
+	cin >> maxA;
+	cout << endl;
+	A = input(n, maxA);
 	cout << "The matrix A:" << endl;
-	output(A);
-	creation(A);
+	output(A, n);
+	B = creation(A, n);
 	cout << "The matrix B:" << endl;
-	output(B);
+	output(B, n);
+	deleting(A, n);
+	deleting(B, n);
 	system("pause");
+	return 0;
 }
 
-void input(Matrix matr)
+float** input(int m, int maxRand)
 {
+	float** matr = new float* [m];
+	for (int i = 0; i < m; i++)
+		matr[i] = new float[m];
 	srand(time(NULL));
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			matr[i][j] = rand() % 50 + 1;
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < m; j++)
+			matr[i][j] = rand() % maxRand + 1;
+	return matr;
 }
 
-void output(Matrix matr)
+void output(float** matr, int m)
 {
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-			cout << setw(7) << setprecision(3) << matr[i][j];
+	float M;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < m; j++) {
+			M = matr[i][j];
+			cout << setw(8) << setprecision((M < 10) ? 2 : 
+				(M < 100) ? 3 : (M < 1000) ? 4 : 6) << M;
+		}
 		printf("\n");
 	}
 	printf("\n");
 }
 
-void creation(Matrix matr)
+float** creation(float** matr, int m)
 {
+	float** matr2 = new float* [m];
+	for (int i = 0; i < m; i++)
+		matr2[i] = new float[m];
 	float Sum;
 	int k;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-		{
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < m; j++) {
 			Sum = 0;
 			k = 0;
-			if (j != 0) 
-			{
-				Sum += A[i][j - 1];
+			if (j != 0) {
+				Sum += matr[i][j - 1];
 				k++;
 			}
-			if (j != n - 1) 
-			{
-				Sum += A[i][j + 1];
+			if (j != m - 1) {
+				Sum += matr[i][j + 1];
 				k++;
 			}
-			if (i != 0) 
-			{
-				Sum += A[i - 1][j];
+			if (i != 0) {
+				Sum += matr[i - 1][j];
 				k++;
 			}
-			if (i != n - 1) 
-			{
-				Sum += A[i + 1][j];
+			if (i != m - 1) {
+				Sum += matr[i + 1][j];
 				k++;
 			}
-			B[i][j] = Sum / k;
+			matr2[i][j] = Sum / k;
 		}
+	return matr2;
+}
+
+void deleting(float** matr, int m)
+{
+	for (int i = 0; i < m; i++)
+		delete[] matr[i];
+	delete[] matr;
 }
