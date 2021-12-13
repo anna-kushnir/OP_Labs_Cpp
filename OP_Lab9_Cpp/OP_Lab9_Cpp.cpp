767837c59ad501aac9f8ea882a1bc7ff6ff70a1e
anna-kushnir <anna.kushnir.1008@gmail.com>
2021-12-13 19:05:23 +0200

    some improvements

--- OP_Lab9_Cpp/OP_Lab9_Cpp.cpp
+++ OP_Lab9_Cpp/OP_Lab9_Cpp.cpp
@@ -6,22 +6,22 @@ using namespace std;
 bool check(string);                            // whether the condition is met (all characters are numbers or spaces)
 int find_words(string, string**);              // defining the list and number of words
 void output_words(string**, int);              // output of each word in a new line
-void search(string**, int, int*, int*);        // search for the largest and smallest words
-string replace(string**, int, int, int);       // swap the smallest and largest words
+void search(string**, int, long*, long*);      // search for the largest and smallest words
+string replace(string**, int, long, long);     // swap the smallest and largest words
 void deleting(string**, int);                  // deleting an array of words
 
 int main()
 {
 	string s;                                  // the input string
-	int n = 20;
+	int n = 40, m = 15;
 	string** words;
 	words = new string * [n];                  // array of words
 	for (int i = 0; i < n; i++) {
-		words[i] = new string [n];
+		words[i] = new string [m];
 	}
 	int numOfWords;
-	int nMax = 0;
-	int nMin = 0;
+	long nMax = 0;
+	long nMin = 0;
 	bool Is;
 	cout << "Enter a string of numbers and spaces: " << endl;
 	getline(cin,s);
@@ -37,7 +37,7 @@ int main()
 	}
 	else
 		cout << "You entered a wrong line! Try again!" << endl;
-	deleting(words,n);
+	deleting(words,m);
 	system("pause");
 	return 0;
 }
@@ -87,14 +87,14 @@ int find_words(const string str, string** st)
 	return num;
 }
 
-void output_words(string** word, int m)
+void output_words(string** word, int k)
 {
-	for (int j = 0; j < m; j++)
+	for (int j = 0; j < k; j++)
 		cout << "  " << word[j][1] << endl;
 	cout << endl;
 }
 
-void search(string** st, int num, int* numOfMax, int* numOfMin)
+void search(string** st, int num, long* numOfMax, long* numOfMin)
 {
 	long str1;
 	long max = stoul(st[0][1]);
@@ -114,7 +114,7 @@ void search(string** st, int num, int* numOfMax, int* numOfMin)
 	cout << "The smallest word is number " << *numOfMin << ": " << min << endl;
 }
 
-string replace(string** st, int num, int numMax, int numMin)
+string replace(string** st, int num, long numMax, long numMin)
 {
 	
 	string str = "";
@@ -128,9 +128,9 @@ string replace(string** st, int num, int numMax, int numMin)
 	return str;
 }
 
-void deleting(string** st, int m)
+void deleting(string** st, int k)
 {
-	for (int i = 0; i < m; i++) {
+	for (int i = 0; i < k; i++) {
 		delete[] st[i];
 	}
 	delete[] st;
